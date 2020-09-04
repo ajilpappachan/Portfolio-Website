08024d92079c63b70a7b04af65a83e50b7b7ce08
ajilpappachan <ajilpappachan@gmail.com>
2020-09-04 22:43:15 +0530

    Added HomeCover

---
+++ src/component/homeCover/HomeCover.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './HomeCover.css'
+import Particles from 'react-particles-js';
+
+const HomeCover = () => {
+    return (
+        <div>
+            <Particles className='particles' id="tsparticles"
+                params={{
+                    background: {
+                        color: {
+                            value: "#08090A",
+                        },
+                    },
+                    fpsLimit: 60,
+                    interactivity: {
+                        detectsOn: "canvas",
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 400,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40,
+                            },
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: false,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outMode: "bounce",
+                            random: false,
+                            speed: 6,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                value_area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            random: true,
+                            value: 5,
+                        },
+                    },
+                    detectRetina: true,
+                }} />
+                <div className='text'>
+                    <h1 className='f3'>"Great things are done by a series of small things brought together"</h1>
+                    <h2 className='f4 tr'>-Vincent van Gogh</h2>
+                </div>
+        </div>
+    );
+}
+
+export default HomeCover;
